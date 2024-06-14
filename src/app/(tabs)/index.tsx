4d2abd30bef269bbe673f56a0a0bf2b19798f932
Shah Ahmed <leadbassist@live.com>
2024-06-14 19:38:42 -0400

    module 1 - FlatList

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 import products from "@/assets/data/products";
 import ProductListItem from "@/src/components/ProductListItem";
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[5]} />
-      <ProductListItem product={products[1]} />
+      {/* <ProductListItem product={products[5]} />
+      <ProductListItem product={products[1]} /> */}
+
+      <FlatList
+        data={products} // array of items that we want to render. the source data file
+        renderItem={({ item }) => <ProductListItem product={item} />} // a function. tells flatlist how EACH item from the array should be rendered
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }} // gap on rows, gap AROUND the container
+        columnWrapperStyle={{ gap: 10 }} // gap on columns
+      />
     </View>
   );
 }
