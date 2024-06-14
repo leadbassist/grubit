95496ff107a75f823491f3047c9ccaa8ba52c505
Shah Ahmed <leadbassist@live.com>
2024-06-14 18:16:06 -0400

    Module 1 - Your first Custom Component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,41 +1,12 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import Colors from "@/src/constants/Colors";
+import { View } from "react-native";
 import products from "@/assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
 
-const product = products[0];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[5]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "Bold",
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1, // calc height based on width
-  },
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import Colors from "@/src/constants/Colors";
+
+const ProductListItem = ({ product }) => {
+  console.log(product);
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "Bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1, // calc height based on width
+  },
+});
