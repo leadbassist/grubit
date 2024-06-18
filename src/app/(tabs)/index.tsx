835d85655163774f226be1dfafd6de6797bb3876
Shah Ahmed <leadbassist@live.com>
2024-06-17 22:36:56 -0400

    Module 1: expo router screen (including nested navigation and fixing some issues)

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,5 @@
-import { View, FlatList } from "react-native";
-import products from "@/assets/data/products";
-import ProductListItem from "@/src/components/ProductListItem";
+import { Redirect } from "expo-router";
 
-export default function MenuScreen() {
-  return (
-    <View>
-      {/* <ProductListItem product={products[5]} />
-      <ProductListItem product={products[1]} /> */}
-
-      <FlatList
-        data={products} // array of items that we want to render. the source data file
-        renderItem={({ item }) => <ProductListItem product={item} />} // a function. tells flatlist how EACH item from the array should be rendered
-        numColumns={2}
-        contentContainerStyle={{ gap: 10, padding: 10 }} // gap on rows, gap AROUND the container
-        columnWrapperStyle={{ gap: 10 }} // gap on columns
-      />
-    </View>
-  );
+export default function TabIndex() {
+  return <Redirect href={"/menu/"} />;
 }
