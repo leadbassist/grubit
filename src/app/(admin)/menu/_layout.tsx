f3a09888b515cc53fe5c91979dbd745111794bff
Shah Ahmed <leadbassist@live.com>
2024-07-28 20:54:48 -0400

    create a product - screen

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -10,7 +10,7 @@ export default function MenuStack() {
         name="index"
         options={{
           headerRight: () => (
-            <Link href="/" asChild>
+            <Link href="/(admin)/menu/create" asChild>
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
