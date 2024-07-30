32506a8ca9715a465eff190813a7657523e3b16f
Shah Ahmed <leadbassist@live.com>
2024-07-29 22:36:35 -0400

    top tab navigator

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -4,6 +4,7 @@ export default function MenuStack() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Orders" }} />
+      <Stack.Screen name="list" options={{ headerShown: false }} />
     </Stack>
   );
 }
