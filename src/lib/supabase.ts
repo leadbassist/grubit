04ed5548a2495f6ee1827613aadf8dd21eb8976d
Shah Ahmed <leadbassist@live.com>
2024-07-29 23:34:08 -0400

    configuring supabase backend

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://oavdnoddfdoasfnbwqzs.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hdmRub2RkZmRvYXNmbmJ3cXpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzMDgzNjgsImV4cCI6MjAzNzg4NDM2OH0.fm4nG9bpFLANDJlkrgc-NAR0mBQwr_8VFnqQssVPUmM';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
